146f3c58ee9d0e4076f178ec73842f178dbab6d5
linxingt <xingtonglin011016@gmail.com>
2023-03-01 18:04:43 +0100

    doctrine inscription

--- src/Controller/InscriptionController.php
+++ src/Controller/InscriptionController.php
@@ -10,17 +10,37 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\Persistence\ManagerRegistry;
 
 class InscriptionController extends AbstractController
 {
     #[Route('/inscription', name: 'app_inscription')]
-    public function index(Request $request): Response
+    public function index(Request $request, ManagerRegistry $doctrine): Response
     {
         $inscription = new InscriptionTask();
 
         $form = $this->createForm(InscriptionType::class, $inscription);
         $form->handleRequest($request);
 
+        if ($form->isSubmitted() && $form->isValid()){
+            $formDatas = $form->getData();
+
+            $mdp = $formDatas->getNum();
+            $mdpConfi = $formDatas->getConfirmeNum();
+            if($mdp==$mdpConfi){
+                $entityManager=$doctrine->getManager();
+
+                $user= new Utilisateur();
+                $user->setNom($formDatas->getNom());
+                $user->setPrenom($formDatas->getPrenom());
+                $user->setNum($mdp);
+                $user->setEmail($formDatas->getEmail());
+
+                $entityManager->persist($user);
+                $entityManager->flush();
+                return $this->redirectToRoute("app_user");
+            }
+        }
         return $this->render('inscription/index.html.twig', [
             'form' => $form->createView(),
         ]);
